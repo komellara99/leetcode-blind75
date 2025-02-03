6eef6e0c286a0849b6a78e32b5e9c308899e5c07
larakomel <komellara99@gmail.com>
2025-02-03 23:30:11 +0100

    leetcode 11 - Container With Most Water

---
+++ solved_problems/LC11Solution.cs
@@ -0,0 +1,26 @@
+public class MaxAreaSolution {
+    public int MaxArea(int[] height) {
+        var currMax = 0;
+        var left = 0;
+        var right = height.Length -1;
+
+        while(left < right){
+            var res = Math.Min(height[left], height[right]) * (right - left);
+            if(res > currMax) currMax = res;
+
+            if(height[left] < height[right]) left++;
+            else right--;
+        }
+        return currMax;
+    }
+}
+
+/*
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+   
+   Find two lines that together with the x-axis form a container, such that the container contains the most water.
+   
+   Return the maximum amount of water a container can store.
+   
+   Notice that you may not slant the container.
+ */
